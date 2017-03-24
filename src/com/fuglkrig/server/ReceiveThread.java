aa75c96fa2d5adaea4bc64948367d08c44c38afc
sennseikun <thomasbo@hotmail.no>
2017-03-24 23:18:22 +0100

    Fixed communication with adding players to OnlinePlayers

--- src/com/fuglkrig/server/ReceiveThread.java
+++ src/com/fuglkrig/server/ReceiveThread.java
@@ -33,6 +33,7 @@ public class ReceiveThread extends Thread {
     public void stopConnection(){
         try {
             inputSocket.close();
+            OnlinePlayers.removePlayer(player);
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -51,12 +52,15 @@ public class ReceiveThread extends Thread {
     public void run(){
         while(running){
             try {
-                System.out.println("ID"+id);
+
+                System.out.println("Players: " + OnlinePlayers.getPlayers());
+
 
                 DataInputStream in;
                 in = new DataInputStream(inputSocket.getInputStream());
                 String message = in.readUTF();
-                Player player;
+
+                System.out.println(message);
 
                 int datatype = -1;
 
@@ -77,13 +81,22 @@ public class ReceiveThread extends Thread {
                         name = json.getString("nick");
                         String value = "1";
                         ArrayList<Player> op = OnlinePlayers.getPlayers();
-                        for (int i = 0; op.size() < i ; i++) {
-                            if (op.get(i).getNick().equals(name)) {
-                                value = "0";
+
+                        if(name.equals("")){
+                            value = "0";
+                        }
+
+                        else if(op != null){
+                            for (int i = 0; op.size() < i ; i++) {
+                                if (op.get(i).getNick().equals(name)) {
+                                    value = "0";
+                                }
                             }
                         }
 
+
                         JSONObject retur = new JSONObject();
+                        retur.put("Datatype","0");
                         retur.put("userValid", value);
                         retur.put("pId", id);
                         retur.put("nick", name);
@@ -91,12 +104,13 @@ public class ReceiveThread extends Thread {
                         if(value.equals("1")){
 
                             player = new Player(name,id,0,inputSocket);
-
                             OnlinePlayers.newPlayer(player);
                         }
 
                         DataOutputStream out = new DataOutputStream(inputSocket.getOutputStream());
                         out.writeUTF(retur.toString());
+
+                        System.out.println("Output is written");
 
                     }catch(JSONException e){
                         e.printStackTrace();
@@ -114,11 +128,12 @@ public class ReceiveThread extends Thread {
                 }
 
                 System.out.println(message);
-                System.out.println("Hello world");
 
 
             } catch (IOException e) {
                 e.printStackTrace();
+                stopConnection();
+                stopThread();
             }
         }
 
