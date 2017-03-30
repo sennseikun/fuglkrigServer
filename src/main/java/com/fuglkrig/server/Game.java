b1da110e851d7513408d049af7b2da161e9c7231
fomox <magnugut@stud.ntnu.no>
2017-03-30 18:17:15 +0200

    fix file ending

--- src/main/java/com/fuglkrig/server/Game.java
+++ src/main/java/com/fuglkrig/server/Game.java
@@ -128,7 +128,7 @@ public class Game extends Thread{
             //this needs to be the same as the number of powerups.
             type = rand.nextInt(8) + 1;
             try {
-                img = ImageIO.read(new File("/javapowerup.bmp").getAbsoluteFile());
+                img = ImageIO.read(new File("/javapowerup.png").getAbsoluteFile());
             } catch (IOException e) {
                 System.out.println("Cant find powerup.bmp");
                 System.out.println(e);
