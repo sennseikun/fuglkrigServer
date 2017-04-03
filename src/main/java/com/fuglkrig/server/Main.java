75691fb9877166074a14bd22036ddce66f71e7b0
fomox <magnugut@stud.ntnu.no>
2017-04-03 11:04:58 +0200

    added dynamic ip support

--- src/main/java/com/fuglkrig/server/Main.java
+++ src/main/java/com/fuglkrig/server/Main.java
@@ -1,12 +1,30 @@
 package com.fuglkrig.server;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         System.out.println("FUGLKRIG server starting...");
         int port = 5555;
+
+        //updates the ip address to the clients
+        URL url = null;
+        try {
+            url = new URL("http://guttormsen.io/fuglkrig/updateip.php?code=fuGlKrig03042017'");
+            Scanner s = new Scanner(url.openStream());
+            System.out.println("updated ip address on server");
+        } catch (Exception e) {
+            System.out.println("couldnt update ip address");
+            e.printStackTrace();
+        }
+
+
+
+
         try {
             Thread t = new RequestHandler(port);
             t.start();
---
+++ updateip.php
@@ -0,0 +1,33 @@
+<?php
+
+function getUserIP()
+{
+    $client  = @$_SERVER['HTTP_CLIENT_IP'];
+    $forward = @$_SERVER['HTTP_X_FORWARDED_FOR'];
+    $remote  = $_SERVER['REMOTE_ADDR'];
+
+    if(filter_var($client, FILTER_VALIDATE_IP))
+    {
+        $ip = $client;
+    }
+    elseif(filter_var($forward, FILTER_VALIDATE_IP))
+    {
+        $ip = $forward;
+    }
+    else
+    {
+        $ip = $remote;
+    }
+
+    return $ip;
+}
+
+if($_GET["code"]) {
+    if ($_GET["code"] == "fuGlKrig") {
+        $user_ip = getUserIP();
+        file_put_contents("ip.txt", $user_ip);
+    }
+}
+
+
+?>
